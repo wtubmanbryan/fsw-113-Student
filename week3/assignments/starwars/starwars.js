14c1f82f0416796e7b96b314b05fb240b7ca19f9
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-04-14 00:29:33 -0400

    Added Star Wars

--- week3/assignments/starwars/starwars.js
+++ week3/assignments/starwars/starwars.js
@@ -1,8 +1,16 @@
 // declare any necessary variables
+queryType = document.getElementById('queryType')
+itemID = document.getElementById('itemID')
 
 // define a function called 'fetchData()' that passes the values from 
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
+
+function fetchData(){
+    queryType = queryType.value
+    itemID = itemID.value
+    getFromSWAPI()
+}
 
 function getFromSWAPI() {
     // assign values to any necessary variables
@@ -23,3 +31,11 @@ function getFromSWAPI() {
 //labels to 'dataLabel1' and 'dataLabel2' elements in starwars.html, as well
 // as the appropriate values from the data object to the 'dataValue1' and 
 // 'dataValue2' elements in starwars.html.
+const updateInfo = data =>{
+    let keys = Object.keys(data)
+    document.querySelector("#dataLabel1").innerText = `${keys[0].replace('_', ' ')}`
+    document.querySelector("#dataValue1").innerText = `${data[keys[0]]}`
+    
+    document.querySelector("#dataLabel2").innerText = `${keys[3].replace('_', ' ')}`
+    document.querySelector("#dataValue2").innerText = `${data[keys[3]]}`  
+}
