fc33b7b8841dc1c122a3d7da221f7137ab6e3db4
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-04-22 01:36:46 -0400

    Added Modules and Classes

--- week5/assignment/jscript.js
+++ week5/assignment/jscript.js
@@ -1,14 +1,55 @@
 // Declare any necessary variables.
+let studentNames = document.getElementById('studentName')
+let className = document.getElementById("className")
+let studentScores = document.getElementById("studentScores") 
+let possibleScores = document.getElementById("possibleScores") 
+
+let btnReset = document.getElementById("reset") 
+let btnPrint = document.getElementById("print") 
+
+let certStudentName = document.getElementById("certStudentName")  
+let certClassName = document.getElementById("certClassName") 
+let certGrade = document.getElementById("certGrade") 
+let newStudent = ""
 
 // Add am evemt listener that responds to the click of the "print" button by calling a function to instantiate
 //  a new student object, and another function to print the certificate.
+btnPrint.addEventListener("click", createNewStudent)
 
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
+btnReset.addEventListener("click",resetAll)
+
+function resetAll(){
+    //reset all elements
+    studentName.value = ""
+    className.value = ""
+    studentScores.value = ""
+    possibleScores.value = ""
+    certStudentName.innerText = ""
+    certClassName.innerText = ""
+    certGrade.innerText = ""
+    
+}
 
 // Create a function that instantiates a new student object with the input from the HTML form.
-
+function createNewStudent(){
+    //Create an instance of the student class
+    newStudent = new Student(studentName.value,className.value,csvString2NumArray(studentScores.value), csvString2NumArray(possibleScores.value))
+    
+    //calling completeCertificate function to fill in the certificate
+    completeCertificate()
+}
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
-
+function completeCertificate(){
+    
+    //add content to certificate
+    certStudentName.innerText = newStudent.getStudentName()
+    certClassName.innerText =newStudent.getStudentClass()
+   certGrade.innerText = newStudent.calLetterGrade()
+}
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
 // You can use this function when instantiating the arrays in the student object.
+
+const csvString2NumArray = (str) => str.split(',').map(e => parseInt(e))
+
--- week5/assignment/student.js
+++ week5/assignment/student.js
@@ -3,3 +3,89 @@
 // - a method that adds up all the student's scores
 // - a method that adds up all the possible scores
 // - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+
+class Student{
+    constructor(sname,sclass,sscores,pscores)
+    {
+        this.studentName = sname
+        this.studentClass = sclass
+        this.studentScores = sscores
+        this.possibleScores = pscores
+    }
+
+    /*setters and getters for the student class */
+    setStudentName(sname){
+        this.studentName = sname
+    }
+
+    getStudentName(){
+        return this.studentName
+    }
+
+    setStudentClass(sclass){
+        this.studentClass = sclass
+    }
+
+    getStudentClass(){
+        return this.studentClass
+    }
+
+    setStudentScores(sscores){
+        this.studentScores = sscores
+    }
+
+    getStudentScores(){
+        return this.studentScores
+    }
+
+    setPossibleScores(pscores){
+        this.possibleScores = pscores
+    }
+
+    getPossibleScores(){
+        return this.possibleScores
+    }
+
+    addScores(){
+        const total = this.studentScores.reduce(function(runningTotal, score){
+            return score + runningTotal
+        }, 0)
+        return  total
+    }
+
+    addPossibleScores(){
+        const possibletotal = this.possibleScores.reduce(function(runningTotal, pscore){
+            return pscore + runningTotal
+        }, 0)
+        return  possibletotal
+    }
+
+    calLetterGrade(){
+        const decGrade = (this.addScores() / this.addPossibleScores()).toFixed(1)
+        switch(decGrade) {
+            case '0.8':
+            case '0.9':
+            case 1:
+              // 80 - 100
+              return "A"
+              break;
+            case '0.7':
+              // 70 - 79
+              return "B"
+              break;
+            case '0.6':
+             // 60 - 69
+              return "C"
+              break;
+            case '0.5':
+             // 50 - 59
+               return "D"
+               break;
+            default:
+              // 0 - 49
+              return " F"
+          }
+    }
+}
+
+
