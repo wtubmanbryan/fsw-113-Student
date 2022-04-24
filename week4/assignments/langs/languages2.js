b2c6018d0086e52901ae935b06d8359f59a91ccd
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-04-24 03:24:16 -0400

    Added Scavenger

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -3,13 +3,31 @@ const lang = 'JavaScript'
 // Create an event listener for the submit button that adds all 'input' elements to 
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
+const submitBtn = document.querySelector('#submit')
+submitBtn.addEventListener("click", chkLang)
 
 
-function chkLang(langs) {
+function chkLang() {
+    const langArray = [document.querySelector('#lang0').value,
+    document.querySelector('#lang1').value,
+    document.querySelector('#lang2').value,
+    document.querySelector('#lang3').value,
+    document.querySelector('#lang4').value,
+    document.querySelector('#lang5').value,
+    document.querySelector('#lang6').value,
+    document.querySelector('#lang7').value]
+
     let result = false
 
     // use an array method to check whether the user included 'JavaScript' in their
     // list of languages
+
+    langArray.some(function(item){
+        if(item.toUpperCase() == ("JAVASCRIPT")){
+            result = true
+        }
+    })
+
 
     let obj = document.querySelector('#TestResult')
     if (result) 
--- week4/assignments/scavenger/scavenger.js
+++ week4/assignments/scavenger/scavenger.js
@@ -1,11 +1,25 @@
 // Create an event listenter for the 'submit' button that calls the combineLists() function
-
+const submitBtn = document.querySelector('#submit')
+submitBtn.addEventListener("click", combineLists)
 // Modify the combineLists() function to create arrays from each scavenger hunt element in the scavenger.html document. 
 // Then use the spread operator to combine these arrays into a single array. 
 // Sort that array and display it in the 'AllItems' element on the scavenger.html document.
 
 function combineLists() {
+    const scavenger0 = csvString2NumArray(document.querySelector('#scavenger-0').value)
+    const scavenger1 = csvString2NumArray(document.querySelector('#scavenger-1').value)
+    const scavenger2 = csvString2NumArray(document.querySelector('#scavenger-2').value)
+    const scavenger3 = csvString2NumArray(document.querySelector('#scavenger-3').value)
+
+    const scavengers = [...scavenger0, ...scavenger1, ...scavenger2, ...scavenger3]
+    scavengers.sort()
+    
+    document.querySelector('#AllItems').innerHTML = scavengers
+    
+
 }
+
+const csvString2NumArray = (str) => str.split(',').map(e => (e))
 
 // The following code is a bonus addition that facilitates data entry on the scavenger.html document.
 // If you first click in one of the textareas, you can then click scavenger hunt items in the unordered
