73e86a5514c59a74f190a8d9d658f13b3c88375f
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-04-26 02:46:42 -0400

    Added Languages

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -4,32 +4,23 @@ const lang = 'JavaScript'
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
 const submitBtn = document.querySelector('#submit')
-submitBtn.addEventListener("click", chkLang)
+submitBtn.addEventListener("click", () =>{
+    const langArrayInputs = [...document.querySelectorAll("input")]
+
+    const langArray = langArrayInputs.map(input => input.value)
+    chkLang(langArray)
+})
 
 
-function chkLang() {
-    const langArray = [document.querySelector('#lang0').value,
-    document.querySelector('#lang1').value,
-    document.querySelector('#lang2').value,
-    document.querySelector('#lang3').value,
-    document.querySelector('#lang4').value,
-    document.querySelector('#lang5').value,
-    document.querySelector('#lang6').value,
-    document.querySelector('#lang7').value]
-
-    let result = false
+function chkLang(langArray) {
+    result = langArray.some(function(item){
+        return item.toUpperCase() == lang.toUpperCase()
+    })
 
     // use an array method to check whether the user included 'JavaScript' in their
     // list of languages
 
-    langArray.some(function(item){
-        if(item.toUpperCase() == ("JAVASCRIPT")){
-            result = true
-        }
-    })
-
-
-    let obj = document.querySelector('#TestResult')
+     let obj = document.querySelector('#TestResult')
     if (result) 
         obj.innerText = `Congratulations!\nYou know ${lang}.`
     else
